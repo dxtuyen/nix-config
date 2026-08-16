a66cd047742a9da5dcdbad0490dbb0375b8b4c10
dxtuyen <tuyendoxuan05@gmail.com>
2026-08-16 18:50:04 +0700

    feat: switch Thunar icons to Papirus-Dark

--- home/gtk.nix
+++ home/gtk.nix
@@ -4,8 +4,8 @@
   gtk = {
     enable = true;
     iconTheme = {
-      name = "Adwaita";
-      package = pkgs.adwaita-icon-theme;
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
     };
     gtk3.extraConfig = {
       "gtk-application-prefer-dark-theme" = 1;
