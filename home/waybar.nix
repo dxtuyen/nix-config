998d097003fc833a43968c6cc4149bab653657d3
dxtuyen <tuyendoxuan05@gmail.com>
2026-08-18 08:04:12 +0700

    ensure waybar starts via sway exec, disable systemd duplicate

--- home/waybar.nix
+++ home/waybar.nix
@@ -3,7 +3,7 @@
 {
   programs.waybar = {
     enable = true;
-    systemd.enable = true;
+    systemd.enable = false; # Chạy qua exec trong sway config để chắc chắn sau reboot
     settings.mainBar = {
       position = "top";
       height = 30;
