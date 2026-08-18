b102a3d1fd15fdca6e0ec44f2cc8775a42901240
dxtuyen <tuyendoxuan05@gmail.com>
2026-08-18 08:09:03 +0700

    revert to plain exec waybar, waybar handles hot-reload itself

--- home/sway.nix
+++ home/sway.nix
@@ -23,8 +23,7 @@
       exec ~/.local/bin/cycle-wallpaper
 
       # Applets & daemons
-      # exec_always + killall để waybar reload khi nhấn $mod+Shift+c (refresh-session)
-      exec_always "${pkgs.procps}/bin/killall waybar; waybar"
+      exec waybar
       exec nm-applet --indicator
       exec blueman-applet
       exec ${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1
