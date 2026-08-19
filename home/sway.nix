dbbcda9c45b9ef2d989c0a72e05245a43c048e7c
dxtuyen <tuyendoxuan05@gmail.com>
2026-08-19 17:34:58 +0700

    feat: reduce to 4 workspaces - 1.study, 2.AI, 3.code, 4.others

--- home/sway.nix
+++ home/sway.nix
@@ -126,18 +126,16 @@
       set $ws1 "1.study"
       set $ws2 "2.AI"
       set $ws3 "3.code"
-      set $ws4 "4.terminal"
-      set $ws5 "5.others"
+      set $ws4 "4.others"
       workspace number $ws1
       workspace number $ws2
       workspace number $ws3
       workspace number $ws4
-      workspace number $ws5
       bindsym $mod+1 workspace number $ws1
       bindsym $mod+2 workspace number $ws2
       bindsym $mod+3 workspace number $ws3
       bindsym $mod+4 workspace number $ws4
-      bindsym $mod+5 workspace number $ws5
+      bindsym $mod+5 workspace number 5
       bindsym $mod+6 workspace number 6
       bindsym $mod+7 workspace number 7
       bindsym $mod+8 workspace number 8
@@ -147,7 +145,7 @@
       bindsym $mod+Shift+2 move container to workspace number $ws2
       bindsym $mod+Shift+3 move container to workspace number $ws3
       bindsym $mod+Shift+4 move container to workspace number $ws4
-      bindsym $mod+Shift+5 move container to workspace number $ws5
+      bindsym $mod+Shift+5 move container to workspace number 5
       bindsym $mod+Shift+6 move container to workspace number 6
       bindsym $mod+Shift+7 move container to workspace number 7
       bindsym $mod+Shift+8 move container to workspace number 8
